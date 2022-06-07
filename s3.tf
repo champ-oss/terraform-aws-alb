41abd2691fe3a9b5d9a7dd64fd6b902e8b2180de
Justin Reeves <reeves122@gmail.com>
2022-06-07 12:02:53 -0500

    fix s3 module source format

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source                       = "git::git@github.com:champ-oss/terraform-aws-s3.git?ref=69c71919ed14515fcba747f8826f2e79c660f8d7"
+  source                       = "github.com/champ-oss/terraform-aws-s3.git?ref=69c71919ed14515fcba747f8826f2e79c660f8d7"
   git                          = var.git
   name                         = "lb"
   protect                      = var.protect
